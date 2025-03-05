4f7aea23bc8857527107457d67aa303f4d8b258c
Ali Hamza <ah0681988@gmail.com>
2025-03-05 21:32:56 +0500

    adding the effect on button while the api response is coming

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const author = document.getElementById("author");
 const newQuote = document.getElementById("newQuote");
 
 const api_url = "https://quotes-api-self.vercel.app/quote";
+let button = document.querySelector('button');
 
 async function getQuote(url) {
+
+  button.disabled = true;
+  button.style.backgroundColor = "rgba(135, 137, 139, 0.855)";
+
+
   const response = await fetch(url);
   var data = await response.json();
 
   quote.innerText = data.quote;
   author.innerText = data.author;
+
+
+  button.disabled = false;
+  button.style.backgroundColor = "rgba(23, 123, 229, 0.855)";
 }
 
 getQuote(api_url);
